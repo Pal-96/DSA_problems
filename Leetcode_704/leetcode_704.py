6440ee50505009dc48a6e8b502a91653339d8cff
Sayantika Pal <pal.sayan@northeastern.edu>
2024-03-14 17:29:56 -0400

    Commiting leetcode problem # 344 and reformatted problem# 412 and 704

--- Leetcode_704/leetcode_704.py
+++ Leetcode_704/leetcode_704.py
@@ -1,15 +1,15 @@
 from typing import List
 
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        si = 0
-        li = len(nums)-1 #5
-        while(si<=li):
-            mi = si + (li-si)//2 #2, 4, 5
-            if(target == nums[mi]):
-                return mi
-            elif (target>nums[mi]):
-                si = mi+1 #5
-            elif (target<nums[mi]):
-                li = mi-1
-        return -1
+
+def search(nums: List[int], target: int) -> int:
+    si = 0
+    li = len(nums) - 1  # 5
+    while si <= li:
+        mi = si + (li - si) // 2  # 2, 4, 5
+        if target == nums[mi]:
+            return mi
+        elif target > nums[mi]:
+            si = mi + 1  # 5
+        elif target < nums[mi]:
+            li = mi - 1
+    return -1
