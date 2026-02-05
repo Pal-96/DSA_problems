d9f2e436351ba366514b67282d50f16cdea284ea
Sayantika Pal <pal.sayan@northeastern.edu>
2026-02-05 15:58:57 -0500

    solved leetcode 433

--- Leetcode_433/leetcode_433.py
+++ Leetcode_433/leetcode_433.py
@@ -1,20 +1,21 @@
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
         dq = deque()
-        dq.append([startGene, 0])
         visited = set()
+        l = len(startGene)
         visited.add(startGene)
-        choices = set(('A', 'C', 'G', 'T'))
+        dq.append([startGene, 0])
+        comb = set(('A', 'C', 'G', 'T'))
         while dq:
-            gen e, steps = dq.popleft()
-            if gene == endGene:
-                return steps
-            
-            for i, s in enumerate(gene):
-                for c in choices:
-                    if s != c:
-                        new_gene = gene[:i] + c + gene[i+1:]
-                        if new_gene in bank and new_gene not in visited:
-                            visited.add(new_gene)
-                            dq.append([new_gene, steps+1])
+            startGene, mutation = dq.popleft()
+            if startGene == endGene:
+                return mutation
+            for i in range(0, l):
+                for c in comb:
+                    if startGene[i] == c:
+                        continue
+                    new_gene = startGene[:i] + c + startGene[i+1:]
+                    if new_gene in bank and new_gene not in visited:
+                        dq.append([new_gene, mutation + 1])
+                        visited.add(new_gene)
         return -1
